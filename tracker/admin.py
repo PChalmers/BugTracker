4a70c31ece24a5f7523b0117f5dd41f10fae93b5
pchal <pchalmers2011@gmail.com>
2019-11-26 15:39:15 -0500

    Updating model

--- tracker/admin.py
+++ tracker/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (
+    account,
     record,
     project,
     recordComment
 )
 
+admin.site.register(account)
 admin.site.register(record)
 admin.site.register(project)
 admin.site.register(recordComment)
