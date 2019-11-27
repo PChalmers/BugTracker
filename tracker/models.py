52641e748b3c282783fd004efd05b5d8834eb4fd
Paul Chalmers <pchalmers2011@gmail.com>
2019-11-27 00:31:19 -0500

    Model changes

--- tracker/models.py
+++ tracker/models.py
@@ -14,8 +14,8 @@ class account(models.Model):
         (LOCKED, 'Locked'),
     ]
     accountID = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=32, blank=True, null=True)
-    description = models.CharField(max_length=256, blank=True, null=True)
+    name = models.CharField(max_length=32, blank=True, default="")
+    description = models.TextField(max_length=256, blank=True, default="")
     email = models.EmailField(max_length=254)
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True)
@@ -39,11 +39,13 @@ class record(models.Model):
     INIT = 'IN'
     OPEN = 'OP'
     BLOCKED = 'BL'
+    RESOLVED = 'RE'
     CLOSED = 'CL'
     RECORD_STATUS_CHOICES = [
         (INIT, 'Initial'),
         (OPEN, 'Opened'),
         (BLOCKED, 'Blocked'),
+        (RESOLVED, 'Resolved'),
         (CLOSED, 'Closed'),
     ]
     REQ = 'RE'
@@ -68,14 +70,14 @@ class record(models.Model):
         choices=RECORD_STATUS_CHOICES,
         default=INIT,
     )
-    title = models.CharField(max_length=32, blank=True, null=True)
-    description = models.CharField(max_length=256, blank=True, null=True)
+    title = models.CharField(max_length=32, blank=True, default="")
+    description = models.TextField(max_length=256, blank=True, default="")
     originator = models.ForeignKey('account', related_name='originator', on_delete=models.DO_NOTHING)
     assigned = models.ForeignKey('account', related_name='assigned', on_delete=models.DO_NOTHING)
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True)
-    comments = models.ForeignKey('recordComment', on_delete=models.CASCADE)
-
+    comments = models.ForeignKey('recordComment', on_delete=models.CASCADE, blank=True, null=True)
+    resolution = models.TextField(max_length=256, blank=True, default="")
 
     def is_opened(self):
         return self.status in self.OPEN
@@ -92,8 +94,8 @@ class record(models.Model):
 
 class project(models.Model):
     projectID = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=64)
-    description = models.CharField(max_length=256, blank=True, null=True)
+    name = models.CharField(max_length=32, blank=True, default="")
+    description = models.TextField(max_length=256, blank=True, default="")
     owner = models.ForeignKey('account', on_delete=models.DO_NOTHING)
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True)
@@ -104,8 +106,8 @@ class project(models.Model):
 
 class recordComment(models.Model):
     commentID = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=32, blank=True, null=True)
-    content = models.CharField(max_length=256, blank=True, null=True)
+    title = models.CharField(max_length=32, blank=True, default="")
+    content = models.TextField(max_length=256, blank=True, default="")
     owner = models.ForeignKey('account', on_delete=models.DO_NOTHING)
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True)
